bf97b6cba3d45b35d0fa9114684bd8b488d1ca54
Arun Kumar M <marunkumar0203@gmail.com>
2025-02-26 14:47:13 +0530

    Login Feature

--- SJITFrontend/src/components/FunctionalComponent/Login.jsx
+++ SJITFrontend/src/components/FunctionalComponent/Login.jsx
@@ -1,18 +1,35 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 function Login() {
+    const navigate = useNavigate();
+    const [email, setMail] = useState("");
+    const [password, setPass] = useState("");
+    const handleLogin = async ( event ) => {
+        event.preventDefault();
+        const res = await axios.post("https://mern-pop.onrender.com/login", {
+            email: email,
+            password: password
+        });
+        const data = res.data;
+        alert(data.message);
+        if(data.isLoggedIn){
+            navigate("/");
+        }else{
+            alert("Try LoggingIn again");
+        }
+    };
     return (
         <div>
-            <form action="">
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" />
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="text" name="password" />
-                </div>
-                <button type="submit"><Link to='/'>Submit</Link></button>
+            <form onSubmit={handleLogin}>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" value={email} onChange={(e) => setMail(e.target.value)} />
+                <br />
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" value={password} onChange={(e) => setPass(e.target.value)} />
+                <br />
+                <button type="submit">Submit</button>
             </form>
         </div>
     );
